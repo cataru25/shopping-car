921ddee5d4dff23d5823f3d6a4143522deb0e630
Catalina Restrepo Uribe <catalina.restrepou@perficient.com>
2023-06-02 19:06:42 -0500

    [Add] get one user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,6 +13,27 @@ const getAllUsers = async (_, res, next) => {
   }
 };
 
+const getOneUser = async (req, res, next) => {
+  const { userId } = req.params;
+  try {
+    const oneUser = await User.find({ _id: userId });
+    if (User.length === 0) {
+      res.status(404).json({
+        status: 404,
+        message: "Action failed. ID not found, the user does not exist",
+      });
+    } else {
+      res.status(200).json({
+        status: 200,
+        message: "Successful action. The user has been found",
+        data: oneUser,
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
 const createUser = async (req, res, next) => {
   const newUser = new User(req.body);
   try {
@@ -53,6 +74,7 @@ const updateUser = async (req, res, next) => {
 
 module.exports = {
   getAllUsers,
+  getOneUser,
   createUser,
   updateUser,
 };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -8,6 +8,7 @@ const USER_URL_BASE = "/api/v1/user";
 userRouter.get("/", welcomePage);
 userRouter.get("/health", healthCheck);
 userRouter.get(USER_URL_BASE, User.getAllUsers);
+userRouter.get(`${USER_URL_BASE}/:userId`, User.getOneUser);
 userRouter.post(USER_URL_BASE, validateCreateUser, User.createUser);
 userRouter.patch(
   `${USER_URL_BASE}/:userId`,
