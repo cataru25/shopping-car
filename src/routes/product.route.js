651512d0fad5f7ca4e4c6fc9285cac4603b167d8
Catalina Restrepo Uribe <catalina.restrepou@perficient.com>
2023-06-15 09:40:47 -0500

    [Add] auth flow

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -3,6 +3,7 @@ const { healthCheck, welcomePage, Product } = require("../controllers");
 const {
   validateCreateProduct,
   validateUpdateProduct,
+  isAuth,
 } = require("../middlewares");
 
 const productRouter = new Router();
@@ -10,18 +11,25 @@ const PRODUCT_URL_BASE = "/api/v1/product";
 
 productRouter.get("/", welcomePage);
 productRouter.get("/health", healthCheck);
+
 productRouter.get(PRODUCT_URL_BASE, Product.getAllProducts);
 productRouter.get(`${PRODUCT_URL_BASE}/:productId`, Product.getOneProduct);
 productRouter.post(
   PRODUCT_URL_BASE,
+  isAuth(1),
   validateCreateProduct,
   Product.createProduct
 );
 productRouter.patch(
   `${PRODUCT_URL_BASE}/:productId`,
+  isAuth(1),
   validateUpdateProduct,
   Product.updateProduct
 );
-productRouter.delete(`${PRODUCT_URL_BASE}/:productId`, Product.deleteProduct);
+productRouter.delete(
+  `${PRODUCT_URL_BASE}/:productId`,
+  isAuth(1),
+  Product.deleteProduct
+);
 
 module.exports = productRouter;
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,20 +1,26 @@
 const { Router } = require("express");
 const { healthCheck, welcomePage, User } = require("../controllers");
-const { validateCreateUser, validateUpdateUser } = require("../middlewares");
+const {
+  validateCreateUser,
+  validateUpdateUser,
+  isAuth,
+} = require("../middlewares");
 
 const userRouter = new Router();
 const USER_URL_BASE = "/api/v1/user";
 
 userRouter.get("/", welcomePage);
 userRouter.get("/health", healthCheck);
-userRouter.get(USER_URL_BASE, User.getAllUsers);
-userRouter.get(`${USER_URL_BASE}/:userId`, User.getOneUser);
+
+userRouter.get(USER_URL_BASE, isAuth(1), User.getAllUsers);
+userRouter.get(`${USER_URL_BASE}/:userId`, isAuth(), User.getOneUser);
 userRouter.post(USER_URL_BASE, validateCreateUser, User.createUser);
 userRouter.patch(
   `${USER_URL_BASE}/:userId`,
+  isAuth(1),
   validateUpdateUser,
   User.updateUser
 );
-userRouter.delete(`${USER_URL_BASE}/:userId`, User.deleteUser);
+userRouter.delete(`${USER_URL_BASE}/:userId`, isAuth(1), User.deleteUser);
 
 module.exports = userRouter;
